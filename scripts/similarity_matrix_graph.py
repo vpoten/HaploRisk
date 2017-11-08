f769ef453c1ee2c2d3dfbb6206179463a14a67b9
vpoten <vpoten@gmail.com>
2017-11-08 20:21:48 +0100

    similarity matrix

--- scripts/similarity_matrix_graph.py
+++ scripts/similarity_matrix_graph.py
@@ -1,6 +1,7 @@
 import numpy as np
 import json
 import os
+import re
 from bokeh.plotting import figure, save, output_file
 from bokeh.models import HoverTool, ColumnDataSource, LinearColorMapper
 
@@ -8,11 +9,31 @@ from bokeh.models import HoverTool, ColumnDataSource, LinearColorMapper
 def generate_similarity_graph(sim_input_json_file, sim_out_html_file, title="similarity matrix"):
     data = json.load(open(sim_input_json_file))
     records = data['records']
+    sim_array = data['similarities']
+    return _generate_similarity_graph(records, sim_array, sim_out_html_file, title=title)
 
+
+def generate_similarity_graph_go(sim_input_json_file, sim_out_html_file, title="similarity matrix"):
+    data = json.load(open(sim_input_json_file))
+
+    # get feature names and sort them
+    names = set(map(lambda d: d['product1']['name'], data))
+    names.update(set(map(lambda d: d['product2']['name'], data)))
+    names = sorted(names)
+
+    # generate records and similarities arrays
+    records = map(lambda n: {'name': n}, names)
+    sim_array = map(lambda d: {'val': d['similarity'], 'x': names.index(d['product1']['name']),
+                               'y': names.index(d['product2']['name'])}, data)
+
+    return _generate_similarity_graph(records, sim_array, sim_out_html_file, title=title)
+
+
+def _generate_similarity_graph(records, sim_array, sim_out_html_file, title="similarity matrix"):
     N = len(records)
     similarities = np.zeros((N, N))
     max_sim = 0
-    for val in data['similarities']:
+    for val in sim_array:
         sim = val['val'] if val['x'] != val['y'] else 0
         if sim > max_sim:
             max_sim = sim
@@ -42,7 +63,7 @@ def generate_similarity_graph(sim_input_json_file, sim_out_html_file, title="sim
         similarity=similarities.flatten(),
     ))
 
-    p = figure(title="Similarity matrix",
+    p = figure(title=title,
                x_axis_location="above", tools="hover,save",
                x_range=list(reversed(names)), y_range=names)
 
@@ -69,7 +90,17 @@ def generate_similarity_graph(sim_input_json_file, sim_out_html_file, title="sim
 
 
 if __name__ == "__main__":
-    base_path = '/home/victor/Escritorio/Genotipado_Alternativo/colocalizacion/output'
-    sim_input_file = os.path.join(base_path, 'output_enrichr_similarities_10K.json')
-    sim_out_file = os.path.join(base_path, "sim_matrix.html")
-    generate_similarity_graph(sim_input_file, sim_out_file)
+    base_path = '/home/victor/Escritorio/Genotipado_Alternativo/colocalizacion/go_distances'
+
+    file_pattern = 'go_similarities_(.+)\.json'
+    prog = re.compile(file_pattern)
+
+    sim_files = [f for f in os.listdir(base_path) if os.path.isfile(os.path.join(base_path, f)) and prog.match(f)]
+
+    for f in sim_files:
+        sim_input_file = os.path.join(base_path, f)
+        suffix = prog.match(f).groups()[0]
+        sim_out_file = os.path.join(base_path, "sim_matrix_%s.html" % suffix)
+        generate_similarity_graph_go(sim_input_file, sim_out_file, title="Similarity matrix %s" % suffix)
+
+    # generate_similarity_graph(sim_input_file, sim_out_file)
